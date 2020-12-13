9816e0dd07661f5f7c4a5e08516c23c8bad0e5f3
shshwdr <cs.vivian04@gmail.com>
2020-12-13 12:27:54 -0600

    cooking logic

--- Scenes/egg.gd
+++ Scenes/egg.gd
@@ -1,7 +1,11 @@
 extends PathFollow2D
 
+onready var rigidbody = $RigidBody2D
+onready var sprite = $RigidBody2D/Sprite
+var speed = 100
 
-var speed = 500
+var cooked_level = 0
+var cook_speed = 0.04
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +13,15 @@ func _ready():
 
 func _physics_process(delta):
 	offset+=delta*speed
+	
+	var collidings = rigidbody.get_colliding_bodies()
+	for i in collidings:
+		if i.get_parent().is_on:
+			cooking(delta)
+			
+func cooking(delta):
+	cooked_level+=delta*cook_speed
+	sprite.modulate = Color(cooked_level,1,1)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- Scenes/table.gd
+++ Scenes/table.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var off_texture = preload("res://art/UI/checkmark.png")
+var on_texture = preload("res://art/UI/cancel.png")
+var is_on = false
 
 
+onready var sprite = $StaticBody2D/sprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +15,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func flip():
+	is_on = not is_on
+	if is_on:
+		sprite.texture = on_texture
+	else:
+		sprite.texture = off_texture
+
+#func _on_Control_mouse_entered():
+#	print("enter table")
+#	if Input.is_mouse_button_pressed(1):  # Left mouse button.
+#		flip()
+
+
+func _on_StaticBody2D_input_event(viewport, event, shape_idx):
+	#print("well")
+	if event.is_action_pressed("click"):
+		print("hmm")
+		flip()
+
+
+func _on_StaticBody2D_mouse_entered():
+	if Input.is_mouse_button_pressed(1):  # Left mouse button.
+		flip()
