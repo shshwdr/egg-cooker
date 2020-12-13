b7ef429acd1deb2143a3fe3bb2dc8cdb8b92ae0d
shshwdr <cs.vivian04@gmail.com>
2020-12-13 13:27:32 -0600

    add egg sprite

--- Scenes/egg.gd
+++ Scenes/egg.gd
@@ -5,7 +5,7 @@ onready var sprite = $RigidBody2D/Sprite
 onready var progress_bar = $ProgressBar
 var speed = 100
 #rare, medium_rare, medium, medium_well, well_done, overcooked
-var cooked_level_upgrade_number = [50,100,200,100]
+var cooked_level_upgrade_number = [25,50,50,100,50]
 var cooked_level = 0
 var cooked_number = 0
 var cook_speed = 10
@@ -13,6 +13,8 @@ var cook_speed = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	progress_bar.value = 0
+	
+	progress_bar.visible = false
 	pass # Replace with function body.
 
 func _physics_process(delta):
@@ -24,11 +26,22 @@ func _physics_process(delta):
 			cooking(delta)
 			
 func cooking(delta):
+	
+	if cooked_level == cooked_level_upgrade_number.size():
+		#burned
+		return
 	cooked_number+=delta*cook_speed
 	if cooked_number>=cooked_level_upgrade_number[cooked_level]:
-		cooked_number-=cooked_level_upgrade_number[cooked_level]
+		cooked_number = 0
 		cooked_level+=1
-	progress_bar.value = cooked_number/float(cooked_level_upgrade_number[cooked_level])*100
+		sprite.frame = cooked_level
+	if cooked_level == cooked_level_upgrade_number.size():
+		#burned
+		pass
+		progress_bar.visible = false
+	else:
+		progress_bar.visible = true
+		progress_bar.value = cooked_number/float(cooked_level_upgrade_number[cooked_level])*100
 	#sprite.modulate = Color(cooked_level,0,0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
