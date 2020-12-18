3072594c6ca2e90ddc4c94a0424671e65c326057
shshwdr <cs.vivian04@gmail.com>
2020-12-18 17:47:27 -0600

    can drag chicken

--- Scenes/chicken.gd
+++ Scenes/chicken.gd
@@ -3,6 +3,7 @@ extends Node2D
 var is_moving = true
 var target_p 
 var speed = 0.5
+var dragging = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +29,13 @@ func fly_to_target():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if dragging:
+		var mousepos = get_global_mouse_position()
+		#print(self.position)
+		self.global_position = Vector2(mousepos.x, mousepos.y)
+		#print(self.position)
+		return
 	if Util.game_end:
 		$AnimationPlayer.play("jump")
 		return
@@ -39,3 +47,29 @@ func _process(delta):
 		pick_position()
 		$AnimationPlayer.play("walk")
 	pass
+
+
+
+
+
+
+	
+
+func try_put_egg():
+	fly_to_target()
+
+
+func _on_Node2D_input_event(viewport, event, shape_idx):
+	if Util.game_end:
+		return
+	#print("well")
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			dragging = true
+			Util.hold_egg = true
+		elif event.button_index == BUTTON_LEFT and !event.pressed:
+			dragging = false
+			Util.hold_egg = false
+			try_put_egg()
+
+
